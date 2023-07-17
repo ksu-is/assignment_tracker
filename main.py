c471bb8b16bc1b43fc7defdf294b2aa5559b33d0
Kaitlyn Morgan <kmorg111@students.kennesaw.edu>
2023-07-17 14:15:02 -0400

    Update main.py
    (1) Corrected formatting issues; (2) Resolved issue with function to retrieve list of courses from user input

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ def my_options():
 
     window.mainloop()
 
+def courses_list(new_entry):
+    courses_list = new_entry.get().split(", ")
+    return courses_list
+
 # option 1: add coursework
 def my_coursework():
     # coursework information form
@@ -64,7 +68,7 @@ def my_coursework():
         # Course
     course_label = Label(assignment_info_frame, text="Course:")
     course_label.grid(row=2, column=0, sticky="e")
-    course_combobox = ttk.Combobox(assignment_info_frame)
+    course_combobox = ttk.Combobox(assignment_info_frame, values=courses_list(courses_entry))
     course_combobox.grid(row=2, column=1)
 
         # Delivery
@@ -142,7 +146,7 @@ def my_calendar():
 
     window.mainloop()
 
-# option 3: view semester courses
+# option 3: view courses
 def my_courses():
     window = Tk()
     window.title("Course List")
@@ -150,9 +154,11 @@ def my_courses():
     frame = Frame(window)
     frame.pack()
 
-    courses_text = Text(frame, width=50, height=10)
-    courses_text.grid(row=0, column=0, padx=10, pady=10)
-    courses_text.insert(END, command=courses_list)
+    courses_listbox = Listbox(frame, text=semester_combobox.get() + " Courses")
+    courses_listbox.grid(row=0, column=0, padx=10, pady=10)
+    for item in courses_list(courses_entry):
+        courses_listbox.insert(END, " " + item)
+    # test --> courses_list(courses_entry) (possibly str(courses_list(courses_entry)), extra space currently before each course)
 
 # home screen
 window = Tk()
@@ -162,39 +168,51 @@ frame = Frame(window)
 frame.pack()
 
     # Welcome Message
-welcome_frame = LabelFrame(frame, text="Welcome Message")
+welcome_frame = LabelFrame(frame)
 welcome_frame.grid(row=0, column=0, padx=10, pady=10)
-welcome_label = Label(welcome_frame, text="Welcome! This is a simple program to help you keep track of your coursework.\nTo begin, please enter your name and the courses you are taking this semester.\nNOTE: Separate courses with commas.")
-welcome_label.grid(row=0, column=0, sticky="w")
+welcome_label = Label(welcome_frame, text="Welcome! This is a simple program to help you keep track of your coursework. To begin, please complete the following fields.\n\nNOTE: Separate courses with commas.", justify="left", wraplength=275)
+welcome_label.grid(row=0, column=0, sticky="news")
 
     # User Information
-user_info_frame = LabelFrame(frame, text="User Information")
-user_info_frame.grid(row=1, column=0, padx=50, pady=10)
+user_info_frame = LabelFrame(frame)
+user_info_frame.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
     # Name
-name_label = Label(user_info_frame, text="Name:")
+name_label = Label(user_info_frame, text="Name:", justify="left")
 name_label.grid(row=1, column=0)
 name_entry = Entry(user_info_frame)
 name_entry.grid(row=1, column=1)
 
-    # Courses
-courses_label = Label(user_info_frame, text="Courses:")
-courses_label.grid(row=2, column=0)
-courses_entry = Entry(user_info_frame)
-courses_entry.grid(row=2, column=1)
+    # Semester
+semester_info_frame = LabelFrame(frame)
+semester_info_frame.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
-def courses_list():
-    courses_list = courses_entry.get().split(",")
-    return courses_list
+    # Courses
+semester_label = Label(semester_info_frame, text="Semester:", justify="left")
+semester_label.grid(row=2, column=0)
+semester_combobox = ttk.Combobox(semester_info_frame, values=["Fall", "Spring", "Summer"], width=10)
+semester_combobox.grid(row=2, column=1)
+today = datetime.date.today()
+academic_year_spinbox = ttk.Spinbox(semester_info_frame, from_=datetime.date.today().year, to=datetime.date.today().year+5, width=4)
+academic_year_spinbox.grid(row=2, column=2)
+
+courses_info_frame = LabelFrame(frame)
+courses_info_frame.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+
+courses_label = Label(courses_info_frame, text="Courses:", justify="left")
+courses_label.grid(row=3, column=0)
+courses_entry = Entry(courses_info_frame)
+courses_entry.grid(row=3, column=1)
+courses_list(courses_entry)
 
 # navigation menu
 navigation_frame = LabelFrame(frame, text="Navigation Menu")
-navigation_frame.grid(row=2, column=0, padx=10, pady=10)
+navigation_frame.grid(row=4, column=0, padx=10, pady=10)
 
-exit_button = Button(navigation_frame, text="Exit", command=window.quit)
-exit_button.grid(row=3, column=0)
+exit_button = Button(navigation_frame, text="Exit", command=window.destroy)
+exit_button.grid(row=4, column=0)
 
 continue_button = Button(navigation_frame, text="Continue", command=my_options)
-continue_button.grid(row=3, column=1)
+continue_button.grid(row=4, column=1)
 
 window.mainloop()
